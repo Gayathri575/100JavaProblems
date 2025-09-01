6e11420d74a6f082a0086e1affed14e1f35af757
Gayathri575 <gayathriii575@gmail.com>
2025-09-02 00:46:51 +0530

    CheckPalindromString

---
+++ src/CheckPalindrom.java
@@ -0,0 +1,16 @@
+public class CheckPalindrom {
+    public static void main(String[] args) {
+        String word = "RaceCar";
+        String reverse = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reverse = reverse + word.charAt(i);
+        }
+
+        if (word.equalsIgnoreCase(reverse)) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
+    }
+}
